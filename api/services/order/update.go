2099b48d5a3fcd2fa2c7d6237760a15cbf808f50
Sumit Thakur <sumit.thakur@brank.as>
2023-05-26 23:13:20 +0530

    Add postman json and validation add in update order

--- api/services/order/update.go
+++ api/services/order/update.go
@@ -2,14 +2,19 @@ package order
 
 import (
 	"context"
+	"log"
 
+	"github.com/SXerox007/OrderServiceApp/constants"
 	"github.com/SXerox007/OrderServiceApp/protos/order"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (s *Svc) UpdateOrderStatus(ctx context.Context, req *order.UpdateOrderStatusRequest) (*order.Order, error) {
-	// TODO: validation for update order status
+	if err := validateUpdateOrder(req); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
 	isUpdated := false
 	var result *order.Order
 	for _, item := range s.orderStatusMap {
@@ -32,4 +37,18 @@ func (s *Svc) UpdateOrderStatus(ctx context.Context, req *order.UpdateOrderStatu
 
 	return result, nil
 
+}
+
+// validate update order
+func validateUpdateOrder(req *order.UpdateOrderStatusRequest) error {
+	if err := validation.ValidateStruct(req,
+		validation.Field(&req.OrderId, validation.Required),
+		validation.Field(&req.Status,
+			validation.In(constants.ORDER_PLACED, constants.ORDER_CANCELLED, constants.ORDER_COMPLETED, constants.ORDER_DISPATCHED).
+				Error("invalid or unsupported status")),
+	); err != nil {
+		log.Println("Error:", err)
+		return status.Error(codes.InvalidArgument, err.Error())
+	}
+	return nil
 }
